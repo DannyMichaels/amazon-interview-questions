3720d361c8221c5d4aadaae2237100e0e55c82fe
Daniel <itzdanielmichael@gmail.com>
2021-04-02 18:33:42 -0400

    create questions and answers challenge

---
+++ questions-and-answers/main.js
@@ -0,0 +1,70 @@
+const questions = [
+  {
+    question: 'question1',
+    answer: 'answer1',
+  },
+  {
+    question: 'question2',
+    answer: 'answer2',
+  },
+  {
+    question: 'question3',
+    answer: 'answer3',
+  },
+  {
+    question: 'question4',
+    answer: 'answer4',
+  },
+];
+
+let currentQuestionHTML = document.getElementById('current-question');
+let currentQuestionNum = 0;
+const finalQuestionNum = questions.length - 1;
+
+const prevQuestionBtn = document.getElementById('prev-btn');
+const nextQuestionBtn = document.getElementById('next-btn');
+
+const fetchCurrentQuestion = () => {
+  // it'll only get the pair from the index that matches currentQuestionNum
+  Array(questions[currentQuestionNum]).forEach(({ question, answer }) => {
+    currentQuestionHTML.textContent = `${question}: ${answer}`;
+  });
+};
+
+const onComponentDidMount = () => {
+  prevQuestionBtn.classList.add('disabled');
+  fetchCurrentQuestion();
+};
+
+// my love for react shows here with that naming, lol.
+
+const onComponentDidUpdate = () => {
+  fetchCurrentQuestion();
+
+  // my own bonus, make color different if button is disabled.
+  if (currentQuestionNum === finalQuestionNum) {
+    return nextQuestionBtn.classList.add('disabled');
+  }
+
+  if (currentQuestionNum === 0) {
+    return prevQuestionBtn.classList.add('disabled');
+  }
+
+  nextQuestionBtn.classList.remove('disabled');
+  prevQuestionBtn.classList.remove('disabled');
+};
+
+onComponentDidMount();
+
+prevQuestionBtn.addEventListener('click', () => {
+  if (currentQuestionNum === 0) return;
+
+  currentQuestionNum--;
+  onComponentDidUpdate();
+});
+
+nextQuestionBtn.addEventListener('click', () => {
+  if (currentQuestionNum === finalQuestionNum) return;
+  currentQuestionNum++;
+  onComponentDidUpdate();
+});
---
+++ questions-and-answers/readme.md
@@ -0,0 +1,8 @@
+This question was in the junior front-end developer Amazon interview.
+
+# the challenge:
+
+make a left and right arrow with a box inbetween them that displays the current question.
+use the questions that are provided in an array to display the questions.
+
+there is no user input, just display the data.
